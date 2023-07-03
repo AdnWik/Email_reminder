ae88d30ebf70abf571bd2b82c77c01da4bfb7662
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-07-03 23:39:39 +0200

    Fix bug in user_test.py

--- email_reminder/user_test.py
+++ email_reminder/user_test.py
@@ -13,15 +13,15 @@ def test_add_user_to_database_positive():
             email_address TEXT NOT NULL
         );
     """
-    first_name = "Schuyler"
-    last_name = "O'Hara"
-    email_address = "Wilfrid17@yahoo.com"
+    i_first_name = "Schuyler"
+    i_last_name = "O'Hara"
+    i_email_address = "Wilfrid17@yahoo.com"
     get_query = """SELECT * FROM users"""
 
     with sqlite3.connect(':memory:') as connection:
         conn = connection
         execute_on_database(conn, create_table_query)
-        User.add_user(conn, first_name, last_name, email_address)
+        User.add_user(conn, i_first_name, i_last_name, i_email_address)
         data = get_data_from_database(conn, get_query)
 
     for user in data:
@@ -32,6 +32,40 @@ def test_add_user_to_database_positive():
         r_email_address = email_address
 
     assert r_user_id == 1
-    assert r_first_name == first_name
-    assert r_last_name == last_name
-    assert r_email_address == email_address
+    assert r_first_name == i_first_name
+    assert r_last_name == i_last_name
+    assert r_email_address == i_email_address
+
+
+def test_add_user_to_database_negative():
+    create_table_query = """DROP TABLE IF EXISTS users;
+
+        CREATE TABLE IF NOT EXISTS users (
+            user_id	INTEGER	PRIMARY KEY	AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email_address TEXT NOT NULL
+        );
+    """
+    i_first_name = "Schuyler"
+    i_last_name = "O'Hara"
+    i_email_address = "Wilfrid17@yahoo.com"
+    get_query = """SELECT * FROM users"""
+
+    with sqlite3.connect(':memory:') as connection:
+        conn = connection
+        execute_on_database(conn, create_table_query)
+        User.add_user(conn, i_first_name, 'Hartmann', i_email_address)
+        data = get_data_from_database(conn, get_query)
+
+    for user in data:
+        user_id, first_name, last_name, email_address = user
+        r_user_id = user_id
+        r_first_name = first_name
+        r_last_name = last_name
+        r_email_address = email_address
+
+    assert r_user_id not in range(2, 11)
+    assert r_first_name == i_first_name
+    assert r_last_name != i_last_name
+    assert r_email_address == i_email_address
